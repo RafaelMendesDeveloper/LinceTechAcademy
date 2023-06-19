0fb8a36be38fe634094e050b8f6e02a4438388c7
RafaelMendesDeveloper <rafael.mendes.negocios@gmail.com>
2023-06-19 15:41:45 -0300

    Utilizando Diversos Tipos de Variáveis

--- index.dart
+++ index.dart
@@ -1,3 +1,13 @@
-void main(){
-    print('Hello, World!');
+void main() {
+  var nomeCompleto =
+      "Rafael Mendes Pinto"; //utiliza-se var para declarar uma variável
+  Object apelido =
+      "Polêmico"; //Object é por cima então utiliza-se para todo o tipo de variável.
+  String nomeCivil = "Rafael"; //String é quando quero declarar de maneira específica que será uma string.
+  dynamic casado = true; //Dynamic autoriza mudar o tipo da variável
+
+  print(nomeCompleto);
+  print(apelido);
+  print(nomeCivil);
+  print(casado);
 }
