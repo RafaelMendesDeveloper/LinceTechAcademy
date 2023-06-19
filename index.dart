6563f7d7bc50b2629805735a3811ae6dbc68e64a
RafaelMendesDeveloper <rafael.mendes.negocios@gmail.com>
2023-06-19 15:53:48 -0300

    Como colocar a variável com o valor de nula

--- index.dart
+++ index.dart
@@ -1,13 +1,11 @@
 void main() {
-  var nomeCompleto =
-      "Rafael Mendes Pinto"; //utiliza-se var para declarar uma variável
-  Object apelido =
-      "Polêmico"; //Object é por cima então utiliza-se para todo o tipo de variável.
-  String nomeCivil = "Rafael"; //String é quando quero declarar de maneira específica que será uma string.
-  dynamic casado = true; //Dynamic autoriza mudar o tipo da variável
+  // var nomeCompleto = "Rafael Mendes Pinto"; utiliza-se var para declarar uma variável
+  // Object apelido ="Polêmico"; Object é por cima então utiliza-se para todo o tipo de variável.
+  // String nomeCivil = "Rafael"; String é quando quero declarar de maneira específica que será uma string.
+  // dynamic casado = true; Dynamic autoriza mudar o tipo da variável
 
-  print(nomeCompleto);
-  print(apelido);
-  print(nomeCivil);
-  print(casado);
+  // PARA TRANSFORMAR A VARIÁVEL EM NULA É OBRIGATÓRIO COLOCAR O ? logo após o uso.
+  //String? NomeEsposa = null;
+
+  
 }
