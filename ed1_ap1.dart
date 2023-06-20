f1e539283bbd30d0a8358c10d87516874b286ebd
Rafael Mendes Pinto <134473088+RafaelMendesDeveloper@users.noreply.github.com>
2023-06-20 13:19:10 -0300

    Add files via upload
    Exercicio 01.

---
+++ ed1_ap1.dart
@@ -0,0 +1,27 @@
+void main() {
+  String nome = "Rafael";
+  String sobrenome = "Mendes Pinto";
+  int idade = 18;
+  bool ativo = true;
+  double peso = 80.2;
+  late String nacionalidade = "Brasileiro";
+
+  print("Nome completo: $nome $sobrenome");
+
+  if (idade >= 18)
+    print("Idade: $idade(maior de idade).");
+  else
+    print("Idade: $idade(menor de idade)");
+
+  if (ativo == true)
+    print("Situação: Ativo");
+  else
+    print("Situação: Inativo");
+
+  print("Peso: $peso");
+
+  if (nacionalidade == "") {
+    print("Nacionalidade: Não Informada");
+  } else
+    print("Nacionalidade: $nacionalidade");
+}
