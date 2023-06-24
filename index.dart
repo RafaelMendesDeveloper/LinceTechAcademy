90451e852b5df37f95cc2203f3345d1ea427cadf
RafaelMendesDeveloper <rafael.mendes.negocios@gmail.com>
2023-06-24 12:14:49 -0300

    Funções exemplificadas e estudadas

--- index.dart
+++ index.dart
@@ -53,34 +53,89 @@ void main() {
   //spread operator funciona!!!
   //map é muito útil, serve pra BASTAAAANTE COISA!
 
-  Map<String, List<String>> livros = Map<String, List<String>>();
+//   Map<String, List<String>> livros = Map<String, List<String>>();
 
-  livros['Harry Potter'] = [
-    'Pedra Filosofal',
-    'Câmara Secreta',
-    'Prisioneiros de Azkhabam',
-    'Cálice de Fogo',
-    'Ordem da Fênix',
-    'Enigma do Príncipe',
-    'Reliquias da Morte'
-  ];
-  livros['Jogos Vorazes'] = ['A Sorte', 'Em Chamas', 'Esperança'];
-  livros['Os Vingadores'] = [
-    'The Avengers',
-    'A Era de Ultron',
-    'Guerra Infinita',
-    'Ultimato'
-  ];
+//   livros['Harry Potter'] = [
+//     'Pedra Filosofal',
+//     'Câmara Secreta',
+//     'Prisioneiros de Azkhabam',
+//     'Cálice de Fogo',
+//     'Ordem da Fênix',
+//     'Enigma do Príncipe',
+//     'Reliquias da Morte'
+//   ];
+//   livros['Jogos Vorazes'] = ['A Sorte', 'Em Chamas', 'Esperança'];
+//   livros['Os Vingadores'] = [
+//     'The Avengers',
+//     'A Era de Ultron',
+//     'Guerra Infinita',
+//     'Ultimato'
+//   ];
 
-  livros.forEach((key, value) {
-    print("""
-      $key
-      ${value}
-""");
-  });
+//   livros.forEach((key, value) {
+//     print("""
+//       $key
+//       ${value}
+// """);
+//   });
 
-  Map<String, List<String>>? teste = null;
+//   Map<String, List<String>>? teste = null;
 
-  var novosLivros = {...livros, ...?teste};
-  print(novosLivros);
+//   var novosLivros = {...livros, ...?teste};
+//   print(novosLivros);
+
+  //FUNÇÕES -> até agora todos os códigos feitos foram realizados dentro da função Main
+  // Function, Funções tem retorno, o nome e os parâmetros.
+  // Declara a function, abre fecha parenteses e declara um bloco
+  // se a função não retorna nada, eu coloco void
+  //quando ela não recebe nenhum parâmetro eu boto function()
+  // 3 TIPOS DIFERENTES DE SE ESCREVER A MESMA FUNÇÃO
+  // void Function() minhaFuncao = () {
+  //   print("Minha Função");
+  // };
+
+  // var minhaFuncao2 = () {
+  //   print("Minha Função");
+  // };
+
+  // void minhaFunçao() {
+  //   print('Minha Função');
+  // };
+
+  //no caso abaixo a getSoma é declarada como uma função de número que receberá
+  //duas variaveis de numero, transformara elas nas variaveis v1 e v2;
+  //e vai dar RETURN na conta proposta
+
+  // num Function(num, num) getSoma = (num v1, num v2) {
+  //   return v1 + v2 / v1;
+  // };
+
+  num getSoma(num v1, num v2) {
+    return v1 + v2 / v1;
+  }
+  //uma forma mais simplificada da outra função acima
+
+  // minhaFuncao();
+  print(getSoma(2, 3));
+
+  //podemos declarar uma função dentro ou fora da outra tranquilamente
+  //por isso não necessáriamente eu preciso deixar minhas funções dentro
+  //do int main
+
+  //PRA SIMPLIFICAR AINDA MAIS A FUNÇÃO ACIMA PODERIAMOS FAZER DESSA MANEIRA
+  num getSoma2(num v1, num v2) => v1 + v2;
+  print(getSoma2(2, 4));
+  print(getSoma2(5, 6));
+
+  num NumFunction(num a, num b) {
+    return a + b;
+  }
+  ;
+
+  num coisa = 8;
+  num ruim = 9;
+
+  print(NumFunction(coisa, ruim));
+
+  //para funções curtas o => funciona como o return
 }
